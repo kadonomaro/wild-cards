bb08877d7535d82af1a84aebff96be32f0c056d4
kadonomaro <denis.afer@gmail.com>
2019-06-08 18:01:42 +0700

    change blocks layout

--- js/randomCards.js
+++ js/randomCards.js
@@ -12,12 +12,13 @@ let randomCards = {
         cards.forEach(card => {
             let cardStr = card.querySelector('.js-card-str');
             let cardDef = card.querySelector('.js-card-def');
+            let cardImage = card.querySelector('.js-body');
             this.data
                 .then((json) => {
                     cardStr.textContent = json.strength[this.random(0, json.strength.length - 1)];
                     cardDef.textContent = json.defence[this.random(0, json.defence.length - 1)];
                     card.style.backgroundImage = `url('${json.frontImage[this.random(0, json.frontImage.length - 1)]}')`;
-                    console.log(json.frontImage[this.random(0, json.frontImage.length - 1)]);
+                    cardImage.style.backgroundImage = `url('${json.image[this.random(0, json.image.length - 1)]}')`;
                 });
 
         });
