5b0af127d9a91c0316b1e9de8eb07fdd49dfe5f1
kadonomaro <denis.afer@gmail.com>
2019-06-12 00:56:09 +0700

    added favicon pack and meta, defeat check

--- js/battleAction.js
+++ js/battleAction.js
@@ -1,8 +1,10 @@
 function battleAction(friendCards, enemyCards) {
+    let round = 0;
+    let endGamePause = 2000;
     
     friendCards.forEach(card => {
         card.addEventListener('click', () => {
-            
+
             card.classList.remove('card--shadow');
             if (card.classList.contains('card--golden-shadow')) {
                 card.classList.remove('card--golden-shadow');
@@ -20,6 +22,7 @@ function battleAction(friendCards, enemyCards) {
 
             switch (true) {
                 case (queue < 0.5):
+                    round++;
                     enemyCard.classList.add('enemy-queue');
 
                     cardDef.textContent =
@@ -30,6 +33,7 @@ function battleAction(friendCards, enemyCards) {
                     }
                     break;
                 case (queue >= 0.5):
+                    round++;
                     card.classList.add('friend-queue');
 
                     enemyCardDef.textContent =
@@ -38,6 +42,22 @@ function battleAction(friendCards, enemyCards) {
                         enemyCard.classList.add('defeated');
                     }
                     break;
+            }
+
+            if (round === 6) {
+                setTimeout(() => {
+                    
+                    let friendDefeated = friendCards.filter(card => {
+                        return card.classList.contains('defeated');
+                    }).length;
+
+                    let enemyDefeated = enemyCards.filter(card => {
+                        return card.classList.contains('defeated');
+                    }).length;
+                    
+
+
+                }, endGamePause);
             }
         }, {once: true});
     });
