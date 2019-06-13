73eb42d6dab1c67687f14dcddfc4e876376eae96
kadonomaro <denis.afer@gmail.com>
2019-06-14 00:01:46 +0700

    added preload screen proto

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,12 @@
 function preloader() {
     let preloaderBlock = document.querySelector('.preloader');
+    let preloaderLeft = preloaderBlock.querySelector('.js-pre-left');
+    let preloaderRight = preloaderBlock.querySelector('.js-pre-right');
 
-
+    preloaderBlock.addEventListener('click', function () {
+        preloaderLeft.classList.add('preloader__left--hidden');
+        preloaderRight.classList.add('preloader__right--hidden');
+    });
 
 }
 
