df3bf25a6d920ff58445c5d9bba4bec2519da687
kadonomaro <denis.afer@gmail.com>
2019-06-15 20:48:46 +0700

    set style/image for localization block, save/load value

--- js/changeLocalization.js
+++ js/changeLocalization.js
@@ -1,6 +1,11 @@
 function changeLocalization() {
     let localLang = document.querySelectorAll('.js-lang');
     localLang.forEach(lang => {
+        
+        if (lang.value === localStorage.getItem('lang')) {
+            lang.setAttribute('checked', true);
+        }
+
         lang.addEventListener('change', () => {
             localStorage.setItem('lang', lang.value);
         });
